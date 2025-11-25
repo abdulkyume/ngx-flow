214da76d66d63dbe75f5e7e8c2a22aca2480fcf7
abdulkyume <abbirtak3@gmail.com>
2025-11-25 23:02:31 +0600

    feat: add alignment and distribution controls component

--- libs/ngx-flow/src/lib/components/alignment-controls/alignment-controls.component.ts
+++ libs/ngx-flow/src/lib/components/alignment-controls/alignment-controls.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, computed, effect } from '@angular/core';
+import { Component, ChangeDetectionStrategy, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DiagramStateService } from '../../services/diagram-state.service';
 
@@ -16,13 +16,7 @@ export class AlignmentControlsComponent {
     // Only show distribution controls if more than 2 nodes are selected
     showDistribution = computed(() => this.diagramStateService.selectedNodes().length > 2);
 
-    constructor(private diagramStateService: DiagramStateService) {
-        // Debug logging
-        effect(() => {
-            console.log('Alignment Controls - Selected Nodes:', this.diagramStateService.selectedNodes().length);
-            console.log('Alignment Controls - Show Controls:', this.showControls());
-        });
-    }
+    constructor(private diagramStateService: DiagramStateService) { }
 
     align(alignment: 'left' | 'center' | 'right' | 'top' | 'middle' | 'bottom'): void {
         this.diagramStateService.alignNodes(alignment);
